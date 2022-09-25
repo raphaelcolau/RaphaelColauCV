019bf6e35d3fa0031e96ee327415f33b765a8bcb
polyster20 <polyster20@gmail.com>
2022-09-25 16:21:18 +0200

    Description added

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -6,8 +6,10 @@ import Logo from '../components/logo';
 
 export default class PageDesktop extends React.Component {
 
+    
+
     render() {
-        return(
+        return(console.log('Je suis un test'),
             <div className="desktop-display">
                 <div className="frame"></div>
                 <div className="topleft-container">
@@ -15,6 +17,13 @@ export default class PageDesktop extends React.Component {
                         <div className='logo-container'>
                             <Logo />
                         </div>
+                        <p className='title'>Freelance developer</p>
+                        <p>Hello, I'm Raphaël</p>
+                        <p>I'm a Fullstack Developper from Paris.</p>
+                        <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
+                        <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                        <p>I am currently a freelance developer.</p>
+                        <p>Formerly a developer at <code>Opasenior</code></p>
                     </div>
                 </div>
                 <div className="content-container">
