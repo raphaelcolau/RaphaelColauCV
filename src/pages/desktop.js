490ff3b3d4220a804ac95442b952826b97c5a67d
polyster20 <polyster20@gmail.com>
2022-09-26 15:21:22 +0200

    fix: desktop height overflow (caused by a position absolute)

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -5,18 +5,17 @@ import Projectlist from '../components/desktop/list';
 import Logo from '../components/logo';
 
 export default class PageDesktop extends React.Component {
-
-    
-
     render() {
         return(console.log('Je suis un test'),
             <div className="desktop-display">
                 <div className="frame"></div>
                 <div className="topleft-container">
                     <div className="content">
+
                         <div className='logo-container'>
                             <Logo />
                         </div>
+
                         <p className='title'>Freelance developer</p>
                         <p>Hello, I'm Raphaël</p>
                         <p>I'm a Fullstack Developper from Paris.</p>
@@ -24,6 +23,7 @@ export default class PageDesktop extends React.Component {
                         <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
                         <p>I am currently a freelance developer.</p>
                         <p>Formerly a developer at <code>Opasenior</code></p>
+                        
                     </div>
                 </div>
                 <div className="content-container">
