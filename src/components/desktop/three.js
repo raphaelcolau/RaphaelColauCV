ebbfcb7882d8cc240ff9dff9951eef8ba77211bd
polyster20 <polyster20@gmail.com>
2022-10-01 12:53:07 +0200

    three.js cleared

--- src/components/desktop/three.js
+++ src/components/desktop/three.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from '@react-three/drei';
-
-function AddMesh() {
-    return(
-        <mesh position={[0, 1.2, 0]}>
-            <boxGeometry attach="geometry" />
-            <meshLambertMaterial attach="material" color="white" />
-        </mesh>
-    )
-}
+import { Stars } from '@react-three/drei';
 
 export default function Three() {
     return(
-        <div className='three-container' onClick={() => {
-            console.log("CLick")
-        }}>
+        <div className='three-container'>
             <Canvas>
-                <OrbitControls />
                 <Stars />
-                <ambientLight intensity={1} />
-                <AddMesh />
             </Canvas>
         </div>
     )
