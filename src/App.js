cf33d482980d56396ddb2323eeb5d3a7e8b16162
polyster20 <polyster20@gmail.com>
2022-10-01 15:34:31 +0200

    Logo and description completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react';
 import PageDesktop from "./pages/desktop";
 import PageMobile from "./pages/mobile";
 import './styles/app.css';
 
+function getWindowDimentions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return ({
+      width,
+      height
+  })
+}
+
 function App() {
+  const [windowDimensions, setWindowD] = useState(getWindowDimentions());
+
+  useEffect(() => {
+      function handleResize() {
+          setWindowD(getWindowDimentions());
+      }
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+  })
+
   return (
     <div className="App">
-      <div className="Desktop">
-        <PageDesktop />
-      </div>
-      <div className="Mobile">
-        <PageMobile />
-      </div>
+      {windowDimensions.width > 768 ? 
+          <div className="Desktop">
+            <PageDesktop />
+          </div>
+          :
+          <div className="Mobile">
+            <PageMobile dimensions={windowDimensions}/>
+          </div>
+        }
     </div>
   );
 }
--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Logo from '../components/logo';
+import '../styles/mobile.css';
 
 export default class PageMobile extends React.Component {
+
     render() {
         return(
-            <>
-                Mobile
-            </>
+            <div className="mobile-display" style={{ '--window-width': this.props.dimensions.width }}>
+                <div className="frame" />
+
+                <div className='top'>
+
+                    <div className='after-logo'>
+                        <div className='logo-container'>
+                            <Logo />
+                        </div>
+                    </div>
+
+                    <div className='description'>
+                        <p className='title'>Freelance developer</p>
+                        <p>Hello, I'm Raphaël</p>
+                        <p>I'm a Fullstack Developper from Paris.</p>
+                        <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
+                        <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                        <p>I am currently a freelance developer.</p>
+                        <p>Formerly a developer at <code>Opasenior</code></p>
+                    </div>
+
+                </div>
+
+                <div className='bottom'>
+                    <div className="link-bar">
+                        Link link link
+                    </div>
+                    <div className='content-list'>
+                        list
+                    </div>
+                </div>
+
+            </div>
         )
     }
 }
