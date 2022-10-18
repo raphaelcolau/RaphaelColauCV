13ad39397ee7c0e7c06dfd6c930db0182150e461
polyster20 <polyster20@gmail.com>
2022-10-18 16:43:32 +0200

    more info button added

--- src/components/modile/list.js
+++ src/components/modile/list.js
@@ -6,7 +6,7 @@ export default class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            positionX: Array(projects.list.length).fill(0)
+            positionX: Array(projects.list.length).fill(50)
         }
         this.handleStop=this.handleStop.bind(this);
         this.handleDrag=this.handleDrag.bind(this);
@@ -23,7 +23,7 @@ export default class List extends React.Component {
     
             positionX[index] = 0; 
             this.setState({ positionX: [...positionX]})
-        }, 500)
+        }, 2000)
     }
 
     handleDrag(e, ui) {
@@ -37,6 +37,10 @@ export default class List extends React.Component {
         positionX[index] += ui.deltaX; 
         this.setState({ positionX: [...positionX]})
     }
+    
+    openModal() {
+        console.log("Clicked")
+    }
 
     render() {
         return(
@@ -44,19 +48,25 @@ export default class List extends React.Component {
                 {projects.list.map((p, index) => {
                     return(
                         <div className='dragzone' key={index}>
+                            <div className='m-under-dragzone' style={{'--scl': Number(this.state.positionX[index])}} onClick={() => {this.openModal()}}>
+                                <>MORE INFO</>
+                            </div>
+
                             <Draggable
                                 axis="x"
                                 bounds={{left: 0, right: 100}}
                                 position={{x: this.state.positionX[index], y: 0}}
                                 onStop={this.handleStop}
-                                onDrag={this.handleDrag}
-                            >
+                                onDrag={this.handleDrag}>
+                                
                                 <div className='p-element-m' id={`${index}`}>
                                     <div>
                                         {p.name}
                                     </div>
                                 </div>
+                                
                             </Draggable>
+
                         </div>
                     )
                 })}
