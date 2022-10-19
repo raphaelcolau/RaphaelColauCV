2006344cd1ccbbe61d5d2e1296f16ea783a32455
polyster20 <polyster20@gmail.com>
2022-10-19 23:14:22 +0200

    link updated

--- src/components/desktop/sidebar.js
+++ src/components/desktop/sidebar.js
@@ -6,16 +6,16 @@ export default function SideBar() {
                 <a href="https://www.linkedin.com/in/raphael-colau-2211b91ba/" rel="noreferrer" target='_blank'>
                     Linkedin
                 </a>
-                <a href="https://github.com/polyster20" rel="noreferrer" target='_blank'>
+                <a href="https://twitter.com/raphaelcolau" rel="noreferrer" target='_blank'>
                     Github
                 </a>
             </div>
 
             <div>
-                <a href="https://twitter.com/polyster20" rel="noreferrer" target='_blank'>
+                <a href="https://github.com/raphaelcolau" rel="noreferrer" target='_blank'>
                     Twitter
                 </a>
-                <a href="mailto: raphaelcolau@yahoo.fr" rel="noreferrer" target='_blank'>
+                <a href="mailto: pro@colau.fr" rel="noreferrer" target='_blank'>
                     Email
                 </a>
             </div>
--- src/components/modile/linkbar.js
+++ src/components/modile/linkbar.js
@@ -5,10 +5,10 @@ export default function Linkbar() {
             <a href="https://www.linkedin.com/in/raphael-colau-2211b91ba/" rel="noreferrer" target='_blank'>
                 Linkedin
             </a>
-            <a href="https://github.com/polyster20" rel="noreferrer" target='_blank'>
+            <a href="https://github.com/raphaelcolau" rel="noreferrer" target='_blank'>
                 Github
             </a>
-            <a href="https://twitter.com/polyster20" rel="noreferrer" target='_blank'>
+            <a href="https://twitter.com/raphaelcolau" rel="noreferrer" target='_blank'>
                 Twitter
             </a>
             <a href="mailto: pro@colau.fr" rel="noreferrer" target='_blank'>
