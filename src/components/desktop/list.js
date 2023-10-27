41c995c990e822a8398f464500b5c230df3792ca
Raphael C <polyster20@gmail.com>
2023-10-27 21:27:29 +0200

    Update list.js
    HoverContext

--- src/components/desktop/list.js
+++ src/components/desktop/list.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../styles/list.css';
+import { HoverContext } from '../context/hoverlist.js';
 
 const projects = require('../../projects.json');
 
 export default function Projectlist() {
+    const { handleHover } = useContext(HoverContext);
+    
     return (
         <div className="list">
             {projects.list.map( (p, index) => {
                 return(
-                    <div className="p-element" key={index}>
+                    <div 
+                        className="p-element"
+                        key={index}
+                        onMouseEnter={() => handleHover(p.name)}
+                        onMouseLeave={() => handleHover(null)}
+                    >
 
                         <div className="text"> {p.name} </div>
 
