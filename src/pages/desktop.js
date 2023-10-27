f3844026f825b6d20dae2ac10a6ed47c87d863a7
Raphael C <polyster20@gmail.com>
2023-10-27 21:26:08 +0200

    Update desktop.js

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -4,49 +4,52 @@ import SideBar from '../components/desktop/sidebar';
 import Projectlist from '../components/desktop/list';
 import Logo from '../components/logo';
 import Three from '../components/desktop/three';
+import HoverProvider from '../components/context/hoverlist';
 
 //TODO Changer la flèche de My Works
 
 export default class PageDesktop extends React.Component {
     render() {
         return(
-            <div className="desktop-display">
-                <div className="frame"></div>
-                <div className="topleft-container">
-                    <div className='content-bar'></div>
-                    <div className="content">
-                        <div className='logo-container'>
-                            <Logo />
-                        </div>
+            <HoverProvider>
+                <div className="desktop-display">
+                    <div className="frame"></div>
+                    <div className="topleft-container">
+                        <div className='content-bar'></div>
+                        <div className="content">
+                            <div className='logo-container'>
+                                <Logo />
+                            </div>
 
-                        <p className='title'>Freelance developer</p>
-                        <p>Hello, I'm Raphaël</p>
-                        <p>I'm a Fullstack Developper from Paris.</p>
-                        <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
-                        <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
-                        <p>I am currently a freelance developer.</p>
-                        <p>Formerly a developer at <code>Opasenior</code></p>
-                        
-                    </div>
-                </div>
-
-                <Three />
-
-                <div className="content-container">
-
-                    <div className="left">
-                        My Works →
-                    </div>
-                
-                    <div className="right">
-                        <div className="project-list">
-                            <Projectlist />
+                            <p className='title'>Freelance developer</p>
+                            <p>Hello, I'm Raphaël</p>
+                            <p>I'm a Fullstack Developper from Paris.</p>
+                            <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
+                            <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                            <p>I am currently a freelance developer.</p>
+                            <p>Formerly a developer at <code>Opasenior</code></p>
+                            
                         </div>
                     </div>
 
+                    <Three />
+
+                    <div className="content-container">
+
+                        <div className="left">
+                            My Works →
+                        </div>
+                    
+                        <div className="right">
+                            <div className="project-list">
+                                <Projectlist />
+                            </div>
+                        </div>
+
+                    </div>
+                    <SideBar />
                 </div>
-                <SideBar />
-            </div>
+            </HoverProvider>
         )
     }
 }
