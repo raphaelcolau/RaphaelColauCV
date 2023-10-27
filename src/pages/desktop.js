66813ecdacce127dcf70bd222731eb6cc6a50f24
Raphael C <polyster20@gmail.com>
2023-10-27 22:36:06 +0200

    class to function component

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -8,48 +8,46 @@ import HoverProvider from '../components/context/hoverlist';
 
 //TODO Changer la flèche de My Works
 
-export default class PageDesktop extends React.Component {
-    render() {
-        return(
-            <HoverProvider>
-                <div className="desktop-display">
-                    <div className="frame"></div>
-                    <div className="topleft-container">
-                        <div className='content-bar'></div>
-                        <div className="content">
-                            <div className='logo-container'>
-                                <Logo />
-                            </div>
-
-                            <p className='title'>Freelance developer</p>
-                            <p>Hello, I'm Raphaël</p>
-                            <p>I'm a Fullstack Developper from Paris.</p>
-                            <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
-                            <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
-                            <p>I am currently a freelance developer.</p>
-                            <p>Formerly a developer at <code>Opasenior</code></p>
-                            
+export default function PageDesktop() {
+    return(
+        <HoverProvider>
+            <div className="desktop-display">
+                <div className="frame"></div>
+                <div className="topleft-container">
+                    <div className='content-bar'></div>
+                    <div className="content">
+                        <div className='logo-container'>
+                            <Logo />
                         </div>
+
+                        <p className='title'>Freelance developer</p>
+                        <p>Hello, I'm Raphaël</p>
+                        <p>I'm a Fullstack Developper from Paris.</p>
+                        <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
+                        <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                        <p>I am currently a freelance developer.</p>
+                        <p>Formerly a developer at <code>Opasenior</code></p>
+                        
                     </div>
-
-                    <Three />
-
-                    <div className="content-container">
-
-                        <div className="left">
-                            My Works →
-                        </div>
-                    
-                        <div className="right">
-                            <div className="project-list">
-                                <Projectlist />
-                            </div>
-                        </div>
-
-                    </div>
-                    <SideBar />
                 </div>
-            </HoverProvider>
-        )
-    }
+
+                <Three />
+
+                <div className="content-container">
+
+                    <div className="left">
+                        My Works →
+                    </div>
+                
+                    <div className="right">
+                        <div className="project-list">
+                            <Projectlist />
+                        </div>
+                    </div>
+
+                </div>
+                <SideBar />
+            </div>
+        </HoverProvider>
+    )
 }
--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -4,47 +4,43 @@ import Linkbar from '../components/modile/linkbar';
 import List from '../components/modile/list';
 import '../styles/mobile.css';
 
-export default class PageMobile extends React.Component {
+export default function PageMobile() {
+    return(
+        <div className="mobile-display" style={{ '--window-width': this.props.dimensions.width }}>
+            <div className="frame" />
 
-    render() {
-        console.log(this.props.dimensions.width)
-        return(
-            <div className="mobile-display" style={{ '--window-width': this.props.dimensions.width }}>
-                <div className="frame" />
+            <div className='top'>
 
-                <div className='top'>
-
-                    <div className='after-logo'>
-                        <div className='logo-container'>
-                            <Logo />
-                        </div>
+                <div className='after-logo'>
+                    <div className='logo-container'>
+                        <Logo />
                     </div>
-
-                    <div className='description'>
-                        <p className='title'>Freelance developer</p>
-                        <p>Hello, I'm Raphaël</p>
-                        <p>I'm a Fullstack Developper from Paris.</p>
-                        <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
-                        <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
-                        <p>I am currently a freelance developer.</p>
-                        <p>Formerly a developer at <code>Opasenior</code></p>
-                    </div>
-
                 </div>
 
-                <div className='bottom'>
-                    <div className="link-bar">
-                        <Linkbar />
-                    </div>
-                    <div className='content-list'>
-                        <div className='left'>
-                            <List />
-                        </div>
-                        <div className='right' />
-                    </div>
+                <div className='description'>
+                    <p className='title'>Freelance developer</p>
+                    <p>Hello, I'm Raphaël</p>
+                    <p>I'm a Fullstack Developper from Paris.</p>
+                    <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
+                    <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                    <p>I am currently a freelance developer.</p>
+                    <p>Formerly a developer at <code>Opasenior</code></p>
                 </div>
 
             </div>
-        )
-    }
+
+            <div className='bottom'>
+                <div className="link-bar">
+                    <Linkbar />
+                </div>
+                <div className='content-list'>
+                    <div className='left'>
+                        <List />
+                    </div>
+                    <div className='right' />
+                </div>
+            </div>
+
+        </div>
+    )
 }
