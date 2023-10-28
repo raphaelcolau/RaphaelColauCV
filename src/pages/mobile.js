b67122f21a980fabbc4a1a520ad4164086a82b6d
Raphael C <polyster20@gmail.com>
2023-10-28 19:55:52 +0200

    English grammar and spelling improvements

--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -18,11 +18,11 @@ export default function PageMobile(props) {
                 </div>
 
                 <div className='description'>
-                    <p className='title'>Freelance developer</p>
+                    <p className='title'>Freelance Developer</p>
                     <p>Hello, I'm Raphaël</p>
-                    <p>I'm a Fullstack Developper from Paris.</p>
-                    <p>I work mostly with <code>Javascript</code> I am working on the <code>Mern</code> stack</p>
-                    <p>I also enjoy design and Adobe Xd holds no secrets for me.</p>
+                    <p>I'm a Full Stack Developer from Paris.</p>
+                    <p>I work primarily with <code>Javascript</code> and I specialize in the <code>MERN</code> stack.</p>
+                    <p>I also enjoy design, and Adobe XD holds no secrets for me.</p>
                     <p>I am currently a freelance developer.</p>
                     <p>Formerly a developer at <code>Opasenior</code></p>
                 </div>
